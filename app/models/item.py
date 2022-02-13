1b07fb43d9d2d00f13e160a556506e24aa953468
tiannaru <tiannaru42@gmail.com>
2022-02-13 22:16:56 +0200

    typo

--- app/models/item.py
+++ app/models/item.py
@@ -15,5 +15,5 @@ class Item(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(255), index=True)
     description = Column(String(255), index=True)
-    owner_id = Column(Integer, ForeignKey("user.id"))
+    owner_id = Column(Integer, ForeignKey("users.id"))
     owner = relationship("User", back_populates="items")
